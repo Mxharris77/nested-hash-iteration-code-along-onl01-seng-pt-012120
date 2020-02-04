097d9a0a187177c95bdc451a4f7e79104c3f882e
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-04 04:45:54 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -29,8 +29,8 @@ contacts = {
 }
 
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
+  contacts.each do |person, contact_details|
+    contact_details.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
      end 
